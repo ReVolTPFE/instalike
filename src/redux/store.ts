efa183fda46c8e7d5c8b8ee93098680a2ef8bcb6
Arnaud Steiner <arnaudsteiner@orange.fr>
2023-02-01 15:53:12 +0100

    redux installed

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore, Middleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import instalikeApi from '../instalikeApi';
+
+const rootReducer = combineReducers({});
+
+const middleware: Middleware[] = [];
+
+middleware.push(thunk.withExtraArgument(instalikeApi));
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
