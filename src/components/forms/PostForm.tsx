9b793e6a204dd29e9a3fc29d3b7cefcdeafa94cc
Arnaud Steiner <arnaudsteiner@orange.fr>
2023-02-07 10:55:42 +0100

    post form added

---
+++ src/components/forms/PostForm.tsx
@@ -0,0 +1,53 @@
+import { useTranslation } from 'react-i18next';
+
+import '../../i18n';
+
+function PostForm() {
+	const { t, i18n } = useTranslation();
+
+	return (
+		<div className="rounded bg-white p-4 border border-red-500">
+			<h2 className="text-center text-xl font-bold">{t('title.postForm')}</h2>
+
+			<form action="" method="post" className="flex flex-row text-center relative">
+				<div className="w-1/2">
+					<input type="file" name="image" id="image" />
+					<img className="w-full aspect-video" src="https://pixy.org/src/487/4870083.jpg" alt="" />
+				</div>
+				<div className="w-1/2 flex flex-col">
+					<textarea
+						className="m-2 p-2 rounded focus:border-blue-500 border border-gray-400"
+						name="description"
+						id="description"
+						placeholder={t('form.postDescription') || 'Write a caption...'}
+					/>
+					<input
+						className="m-2 p-2 rounded focus:border-blue-500 border border-gray-400"
+						type="text"
+						name="localisation"
+						id="localisation"
+						placeholder={t('form.postLocalisation') || 'Localisation'}
+					/>
+
+					<div className="m-2 flex flex-row items-center">
+						<label htmlFor="commentStatus">{t('form.postCommentingStatus') || 'Turn off commenting'}</label>
+						<input className="ml-2" type="checkbox" name="commentStatus" id="commentStatus" />
+					</div>
+
+					<div className="flex flex-row justify-center items-center">
+						<button className="m-2 p-2 rounded hover:bg-gray-200 font-bold cursor-pointer">
+							{t('form.cancel') || 'Cancel'}
+						</button>
+						<input
+							className="m-2 p-2 rounded bg-blue-500 hover:bg-blue-600 text-white font-bold cursor-pointer"
+							type="submit"
+							value={t('form.submit') || 'Submit'}
+						/>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default PostForm;
