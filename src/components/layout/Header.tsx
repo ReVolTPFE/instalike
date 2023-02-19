8883c443f3e0aab9a89244016167473448fe2167
Arnaud Steiner <arnaudsteiner@orange.fr>
2023-02-19 23:19:49 +0100

    added responsive for user menu link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,14 +6,19 @@ import { logoutAsync } from '../../redux/auth/thunks';
 
 function Header() {
 	const [moreInfo, setMoreInfo] = useState(false);
+	const [moreInfoMobile, setMoreInfoMobile] = useState(false);
 
 	function toggleMoreInfo() {
 		setMoreInfo(!moreInfo);
 	}
 
+	function toggleMoreInfoMobile() {
+		setMoreInfoMobile(!moreInfoMobile);
+	}
+
 	const dispatch = useDispatch();
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+	const logout = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		dispatch(logoutAsync());
 	};
@@ -45,7 +50,7 @@ function Header() {
 								}`}
 							>
 								<Link
-									onClick={handleSubmit}
+									onClick={logout}
 									to="#"
 									className="font-sans font-semibold block text-sm my-1 p-1 rounded cursor-pointer hover:bg-red-200 text-red-500"
 								>
@@ -75,8 +80,22 @@ function Header() {
 					<button className="md:hidden mx-4">
 						<i className="text-2xl hover:bg-gray-200 py-1 px-2 text-center rounded fa-solid fa-plus"></i>
 					</button>
-					<button className="md:hidden mx-4">
-						<i className="text-2xl hover:bg-gray-200 py-1 px-2 text-center rounded fa-regular fa-circle-user"></i>
+					<button onClick={toggleMoreInfoMobile} className="md:hidden mx-4 relative">
+						<i className="text-2xl hover:bg-gray-200 py-1 px-2 text-center rounded fa-regular fa-circle-user">
+							<div
+								className={`bg-white border border-gray-200 rounded-md p-2 absolute bottom-0 mb-12 right-0 mt-4 ${
+									moreInfoMobile ? '' : 'hidden'
+								}`}
+							>
+								<Link
+									onClick={logout}
+									to="#"
+									className="font-sans font-semibold block text-sm my-1 p-1 rounded cursor-pointer hover:bg-red-200 text-red-500"
+								>
+									Logout
+								</Link>
+							</div>
+						</i>
 					</button>
 				</nav>
 			</header>
